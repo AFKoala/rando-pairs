ad423d30c604c21c66bac5ced7e51cf92d070f43
AFKoala <bubbles.murph@gmail.com>
2017-07-21 09:19:25 -0400

    July 18 Homework

--- randompair.rb
+++ randompair.rb
@@ -4,12 +4,16 @@ students = []
 # Randomly selects names and pushes them into the 'students' array
 names.length.times do
 	students << names.sample
+	students.uniq
 end
 
 # Splits the 'students' array into a multidimensional array in groups of 2
 pairs = students.each_slice(2).to_a
 
-# Need something to stop duplicates
+# Removes last entry in array so I can reinsert it 
+leftover = pairs.pop.to_s
 
+# Pushes the popped element back into the array 
+pairs[7] << leftover
 
 p pairs
